05b0ec74d8ecace342487128dac6cd7ef6fb98a9
Eduardo Fonseca <efonsecab@gmail.com>
2016-10-19 12:13:16 -0600

    Intro a Programación en Unity

---
+++ UnityProject/IntroUnity/Assets/Scripts/CambiarColorLuz.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CambiarColorLuz : MonoBehaviour
+{
+    private Light objLightComponent = null;
+    // Use this for initialization
+    void Start()
+    {
+        this.objLightComponent = base.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.V))
+        {
+            objLightComponent.color = Color.green;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            objLightComponent.color = Color.white;
+        }
+    }
+}
---
+++ UnityProject/IntroUnity/Assets/Scripts/CambioColor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CambioColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.tag != "EjemploColor")
+            return;
+        if (Input.GetKeyUp(KeyCode.Y))
+        {
+            this.GetComponent<Renderer>().material.color = Color.yellow;
+            Debug.Log("Cambiando color: Amarillo");
+        }
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            this.GetComponent<Renderer>().material.color = Color.green;
+            Debug.Log("Cambiando color: Verde");
+        }
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            this.GetComponent<Renderer>().material.color = Color.blue;
+            Debug.Log("Cambiando color: Azul");
+        }
+        if (Input.GetKeyUp(KeyCode.N))
+        {
+            this.GetComponent<Renderer>().material.color = Color.black;
+            Debug.Log("Cambiando color: Negro");
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            this.GetComponent<Renderer>().material.color = Color.red;
+            Debug.Log("Cambiando color: Rojo");
+        }
+    }
+}
