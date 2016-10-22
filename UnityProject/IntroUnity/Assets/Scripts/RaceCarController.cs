31944bf822e38d86b19daba7f21c1fb6e1fa1451
Eduardo Fonseca <efonsecab@gmail.com>
2016-10-22 12:12:13 -0600

    no message

---
+++ UnityProject/IntroUnity/Assets/Scripts/RaceCarController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaceCarController : MonoBehaviour {
+    public int CurrentSpeed = 0;
+    public int CurrentGear = 1;
+    private int TotalGears = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            int newSpeed = CurrentSpeed + 1;
+            var maxSpeedForCurrentGear = CurrentGear * 20;
+            if (newSpeed > maxSpeedForCurrentGear && CurrentGear < TotalGears)
+                CurrentGear += 1;
+            if (newSpeed <= maxSpeedForCurrentGear)
+                CurrentSpeed = newSpeed;
+        }
+        else
+        {
+            if (CurrentSpeed > 0 )
+                CurrentSpeed = CurrentSpeed-1;
+        }
+        if (CurrentSpeed > 0)
+        {
+            var step = CurrentSpeed * Time.deltaTime;
+            var forward = this.transform.right;
+            //Debug.Log(forward);
+            var newTarget = this.transform.position + forward;
+            //Debug.Log("New Position: " + newTarget);
+            this.transform.position = Vector3.MoveTowards(this.transform.position, newTarget, step);
+        }
+        var h = Input.GetAxis("Horizontal");
+        if (h != 0)
+        {
+            this.transform.Rotate(new Vector3(0,h,0));
+        }
+    }
+}
---
+++ UnityProject/IntroUnity/Assets/Sports Car Multiple textures/Scripts/DemoManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class DemoManager : MonoBehaviour {
+
+	public GameObject[] sportsCars;
+
+	int index;
+
+	public Image[] colorButtons;
+
+	public float rotationSpeed;
+	bool toggle;
+	public MouseOrbit mouseOrbit;
+	Quaternion lastCarRot;
+	void Start () {
+		for (int i = 0; i < sportsCars.Length; i++) {
+			sportsCars [i].SetActive (false);
+		}
+		sportsCars [0].SetActive (true);
+	
+	}
+	
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			toggle = !toggle;
+			if(!toggle)
+				mouseOrbit.enabled = true;
+			else
+				mouseOrbit.enabled = false;
+		}
+
+			if(toggle){
+				sportsCars[index].transform.Rotate(transform.up*rotationSpeed);
+			}
+
+	}
+
+
+	public void showNextCar(){
+		lastCarRot = sportsCars [index].transform.rotation;
+		sportsCars [index].SetActive (false);
+		if (index >= sportsCars.Length - 1)
+			index = 0;
+		else
+		index++;
+		sportsCars [index].SetActive (true);
+		sportsCars [index].transform.rotation=lastCarRot ;
+
+	}
+
+
+	public void showPreviousCar(){
+		lastCarRot = sportsCars [index].transform.rotation;
+		sportsCars [index].SetActive (false);
+		if (index <= 0)
+			index = sportsCars.Length-1;
+		else
+			index--;
+		sportsCars [index].SetActive (true);
+		sportsCars [index].transform.rotation=lastCarRot ;
+	
+	}
+
+	public void changeColor(int no){
+		sportsCars [index].transform.FindChild("body").GetComponent<MeshRenderer> ().materials [0].color = colorButtons[no].color;
+	
+	
+	}
+}
